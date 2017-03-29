c441ea8fdcc14e8fd28848acb324895ec77d86e6
David <dwrowell@gmail.com>
2017-03-29 12:16:14 +0400

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,13 @@
+def collatz(number):
+        while number>1:
+            if number%2==0:
+                number = number//2
+                print(number)
+            elif number%2!=0:
+                number = (3*number+1)
+                print(number)
+        print('COLLLLLAAAATZZZ!!!!!!!')
+     
+print('Please enter a number')
+number = int(input())
+collatz(number)
